5e3707d7a3bc93256e85cfd737920ba3ae07a850
Shambhu Mehta <shambhumehta@gmail.com>
2021-10-06 16:54:43 +0400

    Initial commit - 2

---
+++ src/main/java/com/example/shambhu/aop/SpringBootAopLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.shambhu.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopLoggingApplication.class, args);
+	}
+
+}
